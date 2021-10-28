2a0891d17bf573a12103234293a20655f41a3a81
jahnavi <kosalajahnavi656@gmail.com>
2021-10-28 09:28:43 +0530

    CreateSimpleLinkedList

---
+++ UC-1SimpleLinkedList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            linkedlist list = new linkedlist();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
+
